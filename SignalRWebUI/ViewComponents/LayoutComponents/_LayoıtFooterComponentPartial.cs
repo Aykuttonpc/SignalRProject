6624847dad5cc81aef331e78a7f0ab050dd812ef
Aykut Cıncık <aykutcincik@ogr.eskisehir.edu.tr>
2025-02-14 02:39:23 +0300

    Tema Hazırlığı

---
+++ SignalRWebUI/ViewComponents/LayoutComponents/_LayoıtFooterComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SignalRWebUI.ViewComponents.LayoutComponents
+{
+    public class _LayoıtFooterComponentPartial: ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
